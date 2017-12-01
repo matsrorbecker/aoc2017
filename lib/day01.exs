5991a92bf65bd8038b57b9056f0825c6725ec630
Mats Rörbecker <mats@rorbecker.com>
2017-12-01 14:06:53 +0100

    Refactoring

--- lib/day01.exs
+++ lib/day01.exs
@@ -1,17 +1,29 @@
 defmodule Day01 do
 
-  def solve do
+  def solve_first do
     {:ok, content} = File.read "./input/day01/input.txt"
-    input = String.trim(content)
+    input = to_number_list content
+    [first|_] = input
+    sum_matching input, 0, first
+  end
+
+  def solve_second do
+    {:ok, content} = File.read "./input/day01/input.txt"
+    number_list = to_number_list(content)
+    |> Enum.with_index
+    length = Enum.count number_list
+    half = div(length, 2)
+    input = Enum.map(number_list, fn({val, i}) -> {val, get_val(Enum.at(number_list, rem(i + half, length)))} end)
+    sum_matching2 input, 0
+  end
+
+  defp to_number_list(string) do
+    String.trim(string)
     |> String.split("", trim: true)
     |> Enum.map(&(String.to_integer(&1)))
-    [first|_] = input
-    first_answer = sum_matching input, 0, first
-    indexed_input = Enum.with_index input
-    second_answer = sum_matching2 indexed_input
-    IO.puts first_answer
-    IO.puts second_answer
   end
+
+  defp get_val({val, _}), do: val
 
   defp sum_matching([last|[]], sum, first) when last == first, do: sum + last
   defp sum_matching([_|[]], sum, _), do: sum
@@ -21,17 +33,11 @@ defmodule Day01 do
   end
   defp sum_matching([_|rest], sum, first), do: sum_matching rest, sum, first 
 
-  defp sum_matching2(input) do
-    length = Enum.count input
-    half = div(length, 2)
-    pairs = Enum.map(input, fn({val, i}) -> {val, get_other(Enum.at(input, rem(i + half, length)))} end)
-    sum_matching2 pairs, 0
-  end
   defp sum_matching2([], sum), do: sum
   defp sum_matching2([{first, second}|rest], sum) when first == second do
     new_sum = sum + first
     sum_matching2 rest, new_sum
   end
   defp sum_matching2([_|rest], sum), do: sum_matching2 rest, sum
-  defp get_other({val, _}), do: val
+
 end
